f813992737d622d2723962aa7ca0c4fec70ef2e1
Eddy <edkim220@gmail.com>
2021-03-31 18:37:28 -0400

    finished bfs and max_value problems

--- data-structures-graph-starter-master/lib/breadth_first_search.js
+++ data-structures-graph-starter-master/lib/breadth_first_search.js
@@ -1,5 +1,17 @@
 function breadthFirstSearch(startingNode, targetVal) {
+    let queue = [startingNode];
+    let vals = [];
 
+    while (queue.length) {
+        let currPos = queue.shift();
+
+        if (vals.includes(currPos)) continue;
+        else vals.push(currPos);
+
+        if (currPos.val === targetVal) return currPos;
+        else queue.push(...currPos.neighbors)
+    }
+    return null;
 }
 
 module.exports = {
--- data-structures-graph-starter-master/lib/max_value.js
+++ data-structures-graph-starter-master/lib/max_value.js
@@ -1,5 +1,19 @@
 function maxValue(node, visited=new Set()) {
+    let queue = [node];
+    let vals = [];
 
+    while (queue.length) {
+        let currPos = queue.shift();
+
+        if (vals.includes(currPos.val)) continue;
+        else vals.push(currPos.val);
+
+        if (currPos.neighbors.length) {
+            queue.push(...currPos.neighbors)
+        }
+
+    }
+    return Math.max(...vals)
 }
 
 module.exports = {
