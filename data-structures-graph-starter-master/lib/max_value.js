9efa63a99c12aa5d8c48f611605953fcb5df4db9
Eddy <edkim220@gmail.com>
2021-03-31 19:16:39 -0400

    finished more problems until eod

--- data-structures-graph-starter-master/lib/max_value.js
+++ data-structures-graph-starter-master/lib/max_value.js
@@ -5,15 +5,15 @@ function maxValue(node, visited=new Set()) {
     while (queue.length) {
         let currPos = queue.shift();
 
-        if (vals.includes(currPos.val)) continue;
-        else vals.push(currPos.val);
+        if (vals.includes(currPos.val)) continue;   // checking to make sure value is not already in array, give us value of the current node
+        else vals.push(currPos.val);                // push just the value into vals array
 
-        if (currPos.neighbors.length) {
-            queue.push(...currPos.neighbors)
+        if (currPos.neighbors.length) {         // checking to see if current node has neighbors
+            queue.push(...currPos.neighbors)    // push whole neighbors node into queue array
         }
 
     }
-    return Math.max(...vals)
+    return Math.max(...vals)                    // given set of number, gives max number (highest)
 }
 
 module.exports = {
--- data-structures-graph-starter-master/lib/num_regions.js
+++ data-structures-graph-starter-master/lib/num_regions.js
@@ -1,5 +1,32 @@
 function numRegions(graph) {
+    let count = 0;
 
+    for (let node in graph) {
+        if (graph[node]) {
+            count++;
+        }
+        console.log()
+    }
+    console.log(`this is the count ${count}`);
+    return count;
+}
+
+function BFStraversal(node) {
+    let queue = [node];
+    let vals = [];
+
+    while (queue.length) {
+        let currPos = queue.shift();
+
+        if (vals.includes(currPos.val)) continue;
+        else vals.push(currPos.val);
+
+        if (currPos.neighbors.length) {
+            queue.push(...currPos.neighbors)
+            return true;
+        }
+
+    }
 }
 
 module.exports = {
